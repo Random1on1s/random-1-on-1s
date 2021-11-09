f4d396f667e4389b8d71bade65053ebf85034dbd
Quinn Winters <contact-me@quinnwinters.dev>
2021-11-09 14:45:07 +0100

    feat: add interaction template

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const dotenv = require('dotenv');
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
 dotenv.config();
 
-const client = new Discord.Client();
+const client = new Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+client.once('ready', () => {
+    console.log(`Logging in as ${client.user.tag}`);
 });
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if ( commandName === 'SetSchedule' ) {
+        
+    }
+
+})
 
 
 client.login(process.env.CLIENT_TOKEN)
