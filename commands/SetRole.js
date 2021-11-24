c5a909b0d53a997685c433eeeac80340a325de9f
Quinn Winters <contact-me@quinnwinters.dev>
2021-11-24 14:31:17 +0100

    feat(commands): adding empty SetRole

--- commands/SetRole.js
+++ commands/SetRole.js
@@ -1,2 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 // This will allow you to set the role that the random 1-on-1 bot will use to 
 // choose people for pairings
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('SetRole')
+        .setDescription('Set the role used to find people for 1-on-1s')
+        .addRoleOption(option => 
+            option.setName('Role')
+                .setDescription('The role to be used for random 1-on-1s')
+                .setRequired(true)
+        ),
+    action: async (interaction, client) => {
+        console.log(`Received ${interaction}, going to set default role`);
+        const role = interaction.option.getRole('Role');
+        if (role) {
+            
+            return interaction.reply(`Set the role for random-1-on-1s to ${role}`);
+        } else {
+            return interaction.reply(`No role was provided in ${interaction}`);
+        }
+    },
+}
