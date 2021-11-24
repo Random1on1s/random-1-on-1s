d69fcfc34e65e27c4f27cfcde1ff76ff67a6d159
Quinn Winters <contact-me@quinnwinters.dev>
2021-11-24 14:40:23 +0100

    feat(command): Adding commandConfig framework

--- commands/SetRole.js
+++ commands/SetRole.js
@@ -2,6 +2,21 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // This will allow you to set the role that the random 1-on-1 bot will use to 
 // choose people for pairings
+
+const configPrefix = "ROLE"
+const commandConfig = {
+    prefix: configPrefix,
+    configMessage: role => `${configPrefix}: ${role}`,
+    configParser: msg => {
+        if (msg.startsWith(configPrefix)) {
+            const role = msg.split(": ")[1];
+            return role;
+        } else {
+            return;
+        }
+    }
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('SetRole')
@@ -11,14 +26,15 @@ module.exports = {
                 .setDescription('The role to be used for random 1-on-1s')
                 .setRequired(true)
         ),
-    action: async (interaction, client) => {
+    action: async (interaction, client, config) => {
         console.log(`Received ${interaction}, going to set default role`);
         const role = interaction.option.getRole('Role');
         if (role) {
-            
+            client.channels.cache.get(config.configChannel).send(commandConfig.configMessage(role));
             return interaction.reply(`Set the role for random-1-on-1s to ${role}`);
         } else {
             return interaction.reply(`No role was provided in ${interaction}`);
         }
     },
+    config: commandConfig
 }
