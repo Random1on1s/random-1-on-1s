6bb9f20a6696aa6b3631a2fd4aae059d7231449b
Steven Maio <stevenmaio.321@gmail.com>
2022-03-24 21:52:40 +0100

    feat: handled odd number of pairings
    generate_pairs now handles an odd number of participants. Also added
    more descriptive variable names. Removed redundant fields in Participant
    class.

--- pairing.py
+++ pairing.py
@@ -3,50 +3,48 @@ import argparse
 from scipy.special import softmax
 import numpy as np
 
-from random import choices
+random = np.random.default_rng()
 
 class Participant:
 
-    def __init__(self, num_people, gamma=1/np.e):
-        self.num_people = num_people
+    def __init__(self, num_people):
         self.meetings_counter = np.zeros(num_people)
-        self.log_gamma = np.log(gamma)
 
-    def increment_hits(self, j):
-        self.meetings_counter[j] += 1
+    def increment_hits(self, *other_participants):
+        for other in other_participants:
+            self.meetings_counter[other] += 1
     
-    def sample_match(self, indices):
-        feas = self.log_gamma*self.meetings_counter[indices]
+    def sample_match(self, indices, gamma):
+        feas = gamma*self.meetings_counter[indices]
         probabilities = softmax(feas)
-        match = choices(indices, probabilities, k=1).pop()
+        match = random.choice(indices, p=probabilities)
         return match
 
-def generate_pairs(participants):
+def generate_pairs(participants, gamma):
     unmatched = list(range(len(participants)))
     pairs = []
     while len(unmatched)>1:
-        index = np.random.choice(unmatched)
+        index = random.choice(unmatched)
         unmatched.remove(index)
-        p = participants[index]
-        match = p.sample_match(unmatched)
+        participant = participants[index]
+        match = participant.sample_match(unmatched, gamma)
         unmatched.remove(match)
         pairs.append([index, match])
-    if len(unmatched)==1:
+    if len(unmatched) == 1:
         pairs[0].append(unmatched[0])
     return pairs
 
 # sampling first person based on stuff
 def main(num_people, num_iters, gamma):
-    participants = [Participant(num_people, gamma=gamma) for k in range(num_people)]
-    for i in range(num_iters):
-        pairs = generate_pairs(participants)
-        for i,j in pairs:
-            participants[i].increment_hits(j)
-            participants[j].increment_hits(i)
-    print("average hit/person:", num_iters/(num_people-1))
-    for i,q in enumerate(participants):
-        p = q.meetings_counter
-        print("person {}:".format(i), p)
+    gamma = np.log(gamma)
+    participants = [Participant(num_people) for _ in range(num_people)]
+    for iteration in range(num_iters):
+        pairs = generate_pairs(participants, gamma)
+        for match in pairs:
+            for index in match:
+                participants[index].increment_hits(*filter(lambda i : i != index, match))
+    for participant_no,participant in enumerate(participants):
+        print("participant {}:".format(participant_no), participant.meetings_counter)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='plot example data')
