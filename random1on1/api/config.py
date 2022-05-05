272839593488f34e88b4b2b7b5838b32a79e7d69
qwinters <whisks.wafers-0l@icloud.com>
2022-05-05 17:16:38 +0200

    feat(api): Update various API signatures

--- random1on1/api/config.py
+++ random1on1/api/config.py
@@ -1,31 +1,58 @@
 import json
+from dataclasses import dataclass
+from typing import Union
+
+DEFAULT_ANNOUNCEMENT_CHANNEL = "random-1-on-1-announcements"
+DEFAULT_CATEGORY = "Random 1-on-1s"
+DEFAULT_HISTORY_CHANNEL = "random-1-on-1-bot-history"
+DEFAULT_LOGGING_CHANNEL = "random-1-on-1-bot-logs"
+DEFAULT_ROLE = "Random 1-on-1s"
 
 
+@dataclass(frozen=True)
 class Random1on1BotConfig(object):
-    # TODO: Add configuration values and precondition checks here
 
-    def __init__(
-        self,
-        random1on1_role: str,
-        history_channel: str,
-        logging_channel: str,
-        announcement_channel: str,
-        dm_matches: bool,
-        announce_matches: bool,
-    ):
-        self.random1on1_role = random1on1_role
-        self.history_channel = history_channel
-        self.logging_channel = logging_channel
-        self.announcement_channel = announcement_channel
-        self.dm_matches = dm_matches
-        self.announce_matches = announce_matches
-
-    @classmethod
-    def from_json(cls, json_data: str):
-        return cls(**json.loads(json_data))
+    guild_id: int
+    random1on1_role: str = DEFAULT_ROLE
+    channel_category: str = DEFAULT_CATEGORY
+    announcement_channel: str = DEFAULT_ANNOUNCEMENT_CHANNEL
+    history_channel: str = DEFAULT_HISTORY_CHANNEL
+    logging_channel: str = DEFAULT_LOGGING_CHANNEL
+    announce_matches: bool = True
+    dm_matches: bool = True
 
     def to_json(self) -> str:
         return json.dumps(self,
                           default=lambda obj: obj.__dict__,
                           sort_keys=True,
                           indent=4)
+
+
+def config_from_json(json_data: Union[str, dict]) -> Random1on1BotConfig:
+    if isinstance(json_data, str):
+        return config_from_dict(dictionary=json.loads(json_data))
+    else:
+        return config_from_dict(dictionary=json_data)
+
+
+def validate_announcement_prefs(**dictionary):
+    if "dm_matches" in dictionary and "announce_matches" in dictionary:
+        if not (isinstance(dictionary["announce_matches"], bool)
+                and isinstance(dictionary["dm_matches"], bool)):
+            raise TypeError(
+                "announce_matches and dm_matches must be type 'bool'")
+        return dictionary["announce_matches"] or dictionary["dm_matches"]
+
+
+def config_from_dict(**dictionary) -> Random1on1BotConfig:
+    validate_announcement_prefs(**dictionary)
+    return Random1on1BotConfig(
+        guild_id=dictionary["guild_id"],
+        random1on1_role=dictionary["random1on1_role"],
+        channel_category=dictionary["channel_category"],
+        announcement_channel=dictionary["announcement_channel"],
+        history_channel=dictionary["history_channel"],
+        logging_channel=dictionary["logging_channel"],
+        announce_matches=dictionary["announce_matches"],
+        dm_matches=dictionary["dm_matches"],
+    )
--- random1on1/api/pairings.py
+++ random1on1/api/pairings.py
@@ -1,19 +1,59 @@
+import json
 from datetime import datetime
+from typing import Union
 
+from discord import Guild
 from networkx import Graph
 
 
-class Pairings(object):
-    """
-    Wrapper class to serialize and deserialization information about a set of a pairings
-    """
+class Pairings:
 
     def __init__(
         self,
-        pairings_tuple_list: Graph,
+        pairing_graph: Graph,
         date_of_pairing: datetime,
         dry_run: bool,
     ):
-        self.pairings_tuple_list = pairings_tuple_list
+        self.pairing_graph = pairing_graph
         self.date_of_pairing = date_of_pairing
         self.dry_run = dry_run
+
+    def to_json(self) -> dict:
+        json_dict = {
+            "dry_run":
+            self.dry_run,
+            "date_of_pairing":
+            self.date_of_pairing.strftime("%Y-%m-%d"),
+            "pairing_graph":
+            [(edge[0].id, edge[1].id) for edge in self.pairing_graph.edges],
+        }
+        return json_dict
+
+
+async def pairings_from_json(json_data: Union[str, dict],
+                             guild: Guild) -> Pairings:
+    if isinstance(json_data, str):
+        return await pairings_from_dict(dictionary=json.loads(json_data),
+                                        guild=guild)
+    else:
+        return await pairings_from_dict(dictionary=json_data, guild=guild)
+
+
+async def pairings_from_dict(dictionary: dict, guild: Guild) -> Pairings:
+
+    dry_run = dictionary["dry_run"]
+    date_of_pairing = datetime.strptime(dictionary["date_of_pairing"],
+                                        "%Y-%m-%d")
+
+    pairing_edges = []
+    for person_1_id, person_2_id in dictionary["pairing_graph"]:
+        person_1 = guild.get_member(person_1_id)
+        person_2 = guild.get_member(person_2_id)
+        pairing_edges.append((person_1, person_2))
+
+    pairing_graph = Graph()
+    pairing_graph.add_edges_from(pairing_edges)
+
+    return Pairings(pairing_graph=pairing_graph,
+                    date_of_pairing=date_of_pairing,
+                    dry_run=dry_run)
