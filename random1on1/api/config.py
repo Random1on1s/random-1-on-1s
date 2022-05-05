ae195fc82c4f23b7c2a46d9dcf268059559a4be3
Restyled.io <commits@restyled.io>
2022-05-05 18:09:41 +0000

    style: Restyled by yapf

--- random1on1/api/config.py
+++ random1on1/api/config.py
@@ -24,7 +24,12 @@ class Random1on1BotConfig(object):
     dm_matches: bool = True
 
     def __post_init__(self):
-        validate_announcement_prefs(**{'guild_id': self.guild_id, 'dm_matches': self.dm_matches, 'announce_matches': self.announce_matches})
+        validate_announcement_prefs(
+            **{
+                'guild_id': self.guild_id,
+                'dm_matches': self.dm_matches,
+                'announce_matches': self.announce_matches
+            })
 
     def to_json(self) -> str:
         return json.dumps(self,
@@ -48,8 +53,10 @@ def validate_announcement_prefs(**dictionary):
                 or isinstance(dictionary["dm_matches"], bool)):
             raise ValueError(
                 "announce_matches and dm_matches must be type 'bool'")
-        if dictionary["announce_matches"] == False and dictionary["dm_matches"] == False:
-            raise ValueError("announce_matches and dm_matches cannot both be false") 
+        if dictionary["announce_matches"] == False and dictionary[
+                "dm_matches"] == False:
+            raise ValueError(
+                "announce_matches and dm_matches cannot both be false")
 
 
 def config_from_dict(dictionary) -> Random1on1BotConfig:
@@ -58,9 +65,13 @@ def config_from_dict(dictionary) -> Random1on1BotConfig:
         guild_id=dictionary["guild_id"],
         random1on1_role=dictionary.get("random1on1_role", DEFAULT_ROLE),
         channel_category=dictionary.get("channel_category", DEFAULT_CATEGORY),
-        announcement_channel=dictionary.get("announcement_channel", DEFAULT_ANNOUNCEMENT_CHANNEL),
-        history_channel=dictionary.get("history_channel", DEFAULT_HISTORY_CHANNEL),
-        logging_channel=dictionary.get("logging_channel", DEFAULT_LOGGING_CHANNEL),
-        announce_matches=dictionary.get("announce_matches", DEFAULT_ANNOUNCE_MATCHES),
+        announcement_channel=dictionary.get("announcement_channel",
+                                            DEFAULT_ANNOUNCEMENT_CHANNEL),
+        history_channel=dictionary.get("history_channel",
+                                       DEFAULT_HISTORY_CHANNEL),
+        logging_channel=dictionary.get("logging_channel",
+                                       DEFAULT_LOGGING_CHANNEL),
+        announce_matches=dictionary.get("announce_matches",
+                                        DEFAULT_ANNOUNCE_MATCHES),
         dm_matches=dictionary.get("dm_matches", DEFAULT_DM_MATCHES),
     )
--- test/api/test_config.py
+++ test/api/test_config.py
@@ -3,16 +3,15 @@ import pytest
 from random1on1.api.config import Random1on1BotConfig
 from random1on1.api.config import config_from_json
 
-
 EMPTY_CONFIG_STR = '{}'
 TEST_CONFIG_STR = '{ "guild_id": 1, "dm_matches": false, "announce_matches": true, "history_channel": "hist"}'
 
+
 def test_config_validation():
     with pytest.raises(ValueError):
-        _ = Random1on1BotConfig(
-            guild_id=1,
-            announce_matches=False, 
-            dm_matches=False)
+        _ = Random1on1BotConfig(guild_id=1,
+                                announce_matches=False,
+                                dm_matches=False)
 
 
 def test_config_deserialization_missing_guild():
@@ -26,4 +25,3 @@ def test_config_deserialization_proper_config():
     assert test_config.dm_matches == False
     assert test_config.announce_matches == True
     assert test_config.history_channel == "hist"
-
